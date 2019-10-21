9ee0ef5c6e2047d05573fd1e4a41e4b115b890ba
boamahn <boamahn@uni.coventry.ac.uk>
2019-10-21 18:58:49 +0100

    initial page header

---
+++ front-end/src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import { PageHeader } from 'antd';
+
+ReactDOM.render(
+    <PageHeader
+        style={{color: 'green'}}
+        title = 'Login activity project'
+    />
+);
