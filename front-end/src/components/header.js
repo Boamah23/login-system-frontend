c70ab9b685650da89bf211ae02d7d4c2e9a69811
boamahn <boamahn@uni.coventry.ac.uk>
2019-10-22 13:24:58 +0100

    fixes

--- front-end/src/components/header.js
+++ front-end/src/components/header.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import { PageHeader } from 'antd';
 
-ReactDOM.render(
-    <PageHeader
-        style={{color: 'green'}}
-        title = 'Login activity project'
-    />
-);
+
+function header(){
+    return(
+        <PageHeader
+            style={{color: 'green'}}
+            title = 'Login activity project'
+        />
+    );
+}
+
+export default header;
