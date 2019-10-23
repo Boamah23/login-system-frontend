9d07f40dd4d9e34379cb7d6e6405d45adb0889f8
boamahn <boamahn@uni.coventry.ac.uk>
2019-10-23 03:03:38 +0100

    login form updates

--- front-end/src/components/pages/login.js
+++ front-end/src/components/pages/login.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import '../../style/login.css';
 import { Form, Icon, Input, Button} from 'antd';
@@ -8,35 +7,36 @@ import { Form, Icon, Input, Button} from 'antd';
  function Login() {
      return (
         <div className="form">
-        <Form className="login-form">
-            <Form.Item>
-            <Input
-                prefix={<Icon type="user" style={{ color: "grey" }} />}
-                placeholder="Username"
-            />
-            </Form.Item>
-    
-            <Form.Item>
-            <Input
-                prefix={<Icon type="lock" style={{ color: "grey" }} />}
-                type="password"
-                placeholder="Password"
-            />
-            </Form.Item>
-    
-            <Button type="link" className="login-form-forgot">
-            Forgot password?
-            </Button>
-    
-            <Form.Item>
-            <Button type="primary" htmlType="submit" className="login-form-button">
-                Login
-            </Button>
-    
-            <Button type="link" className="btn">
-                Register
-            </Button>
-            </Form.Item>
+            <div className="title">Login</div>
+            <Form className="login-form">
+                <Form.Item>
+                <Input
+                    prefix={<Icon type="user" style={{ color: "grey" }} />}
+                    placeholder="Username"
+                />
+                </Form.Item>
+        
+                <Form.Item>
+                <Input
+                    prefix={<Icon type="lock" style={{ color: "grey" }} />}
+                    type="password"
+                    placeholder="Password"
+                />
+                </Form.Item>
+        
+                <Button type="link" className="login-form-forgot">
+                Forgot password?
+                </Button>
+        
+                <Form.Item>
+                <Button type="primary" htmlType="submit" className="login-form-button">
+                    Login
+                </Button>
+        
+                <Button type="link" className="btn">
+                    Register
+                </Button>
+                </Form.Item>
         </Form>
         </div>
   );
