09347de06aaf1ffed9f78bf1e4df5d8dbf927b26
boamahn <boamahn@uni.coventry.ac.uk>
2019-10-23 02:46:21 +0100

    register form additions

--- front-end/src/components/pages/register.js
+++ front-end/src/components/pages/register.js
@@ -2,17 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
 import "../style/register.css";
-import { Form, Icon, Input, Button } from "antd";
+import { Form, Icon, Input, Button, Upload } from "antd";
 import { DatePicker } from "antd";
 import moment from "moment";
 
 
 const dateFormat = "DD/MM/YYYY";
 
+const props = {
+  name: 'file',
+  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  headers: {
+    authorization: 'authorization-text',
+  },
+  onChange(info) {
+    if (info.file.status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  }
+};
+
 function Register() {
 
 return (
   <div className="register">
+    <div className="title">Register</div>
     <Form className="register-form">
       <Form.Item>
         <Input placeholder="First name" />
@@ -24,6 +43,7 @@ return (
         <div>
         <div><h4>Date of birth:</h4></div>
           <DatePicker
+            placeholder="Date of birth"
             defaultValue={moment("01/01/2019", dateFormat)}
             Format={dateFormat}
           />
@@ -35,6 +55,11 @@ return (
           placeholder="Email"
         />
       </Form.Item>
+      <Upload {...props}>
+        <Button>
+          <Icon type="upload" /> Click to Upload
+        </Button>
+      </Upload>
       <Form.Item>
         <Input
           prefix={<Icon type="user" style={{ color: "grey" }} />}
