50ccb026d4bc85667c406fadcd3d9f53ad3592b8
boamahn <boamahn@uni.coventry.ac.uk>
2019-11-04 13:17:50 +0000

    login form updated

--- front-end/src/components/pages/login.js
+++ front-end/src/components/pages/login.js
@@ -4,74 +4,69 @@ import '../../style/login.css';
 import { Form, Icon, Input, Button } from 'antd';
 import { Link } from 'react-router-dom';
 
+class LoginForm extends React.Component {
+    handleSubmit = e => {
+      e.preventDefault();
+      this.props.form.validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values);
 
-function Login(props) {
-    const [user, setUser] = useState("")
-    const [pass, setPass] = useState("")
-
-    return (
-        <div className="form">
-            <Form className="login-form">
-                <Form.Item>
-                    <Input
-                        prefix={<Icon type="user" style={{ color: "grey" }} />}
-                        placeholder="Username"
-                        onChange={e => setUser(e.target.value)}
-                    />
-                </Form.Item>
-
-                <Form.Item>
-                    <Input
-                        prefix={<Icon type="lock" style={{ color: "grey" }} />}
-                        type="password"
-                        placeholder="Password"
-                        onChange={e => setPass(e.target.value)}
-                    />
-                </Form.Item>
-                <Link to="">
-                    <Button type="link" className="login-form-forgot">
-                        Forgot password?
-                    </Button>
-                </Link>
-                <Form.Item>
-                    <Button type="primary" htmlType="submit" className="login-form-button" onClick={submitLog}>
-                        Login
-                </Button>
-                    <Link to="/register">
-                        <Button type="link" className="btn">
-                            Register
-                    </Button>
-                    </Link>
-                </Form.Item>
-            </Form>
-        </div>
-    );
-
-    async function submitLog(event) {
-        event.preventDefault();
-        const request = `{"user": "${user}", "pass": "${pass}"}`
-        fetch("api/v1.0/users/", {
-            method: "POST",
-            body: request,
+          fetch('http://localhost:3000/api/v1.0/users', {
+            method: 'POST',
             headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json",
-                "Authorization" : "Basic " + window.btoa(username + ':' + password)
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization' : 'Basic ' + window.btoa(username + ':' + password)
             }
         })
-            .then(data => {
-                props.history.push("/")
-            }
+            .then(data => this.checkResponse(data))
+                
+        }
+      });
+    };
 
-            )
-    }  
-      
-      
-      
-      
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        return (
+          <Form onSubmit={this.handleSubmit} className="login-form">
+            <Form.Item>
+              {getFieldDecorator('username', {
+                rules: [{ required: true, message: 'Please input your username!' }],
+              })(
+                <Input
+                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  placeholder="Username"
+                />,
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('password', {
+                rules: [{ required: true, message: 'Please input your Password!' }],
+              })(
+                <Input
+                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  type="password"
+                  placeholder="Password"
+                />,
+              )}
+            </Form.Item>
+            <Form.Item>
+              <a className="login-form-forgot" href="">
+                Forgot password
+              </a>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                Log in
+              </Button>
+              Or <a href="/">register now!</a>
+            </Form.Item>
+          </Form>
+        );
 
+    }
 }
 
+
+const Login = Form.create({ name: 'login' })(LoginForm); 
  
 
 export default Login;
