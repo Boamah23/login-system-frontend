df79f614771b22116219a3502389959a373c99f3
boamahn <boamahn@uni.coventry.ac.uk>
2019-12-11 12:45:36 +0000

    account path added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Login from './components/pages/login';
 import Register from './components/pages/register';
 import Header from './components/header';
+import Account from './components/pages/account'
 import './components/header';
 import {BrowserRouter as Router, Route
 } from "react-router-dom";
@@ -14,6 +15,7 @@ function App() {
     <Router>
       <Route exact path="/" component={Login}/>
       <Route exact path="/register" component={Register}/>
+      <Route exact path="/account" component={Account}/>
     </Router>
     </>
 
