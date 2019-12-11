548f1e2059eedaa28e62f438981bed6805024bae
boamahn <boamahn@uni.coventry.ac.uk>
2019-12-11 12:46:31 +0000

    password reset UI

---
+++ front-end/src/components/resetPass.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "antd/dist/antd.css";
+import { Form, Icon, Input, Button } from "antd";
+import FormItem from "antd/lib/form/FormItem";
+
+function passwordReset(){
+    return(
+        <div className="forgot">
+        <Form className="forgot-form">
+            <h1 style={{textAlign: 'center'}}>Reset Password</h1>
+            <FormItem>
+                    <Input placeholder="Enter Email" prefix={<Icon type="mail" style={{color: "grey"}}/>}/>
+            </FormItem>
+
+            <FormItem>
+                    <Input.Password placeholder="New Password" prefix={<Icon type="lock" style={{color: "grey"}}/>}/>
+            </FormItem>
+
+            <FormItem>
+                    <Input.Password placeholder="Confirm Password" prefix={<Icon type="mail" style={{color: "grey"}}/>}/>
+            </FormItem>
+
+            <Form.Item className="register-btn">
+                <Button type="primary" htmlType="submit">
+                    Update Password
+                </Button>
+            </Form.Item>
+        </Form>
+
+    </div>
+
+    );
+
+}
+
+export default passwordReset;
