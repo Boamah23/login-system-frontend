3341b5d4eb380f49422943f422653202c0e6f8f4
boamahn <boamahn@uni.coventry.ac.uk>
2019-12-14 20:49:10 +0000

    new routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import Login from './components/pages/login';
 import Register from './components/pages/register';
 import Header from './components/header';
-import './components/header';
+import forgotPass from './components/pages/forgotPass';
+import resetPass from './components/pages/resetPass';
 import {BrowserRouter as Router, Route
 } from "react-router-dom";
 
@@ -14,6 +15,8 @@ function App() {
     <Router>
       <Route exact path="/" component={Login}/>
       <Route exact path="/register" component={Register}/>
+      <Route exact path="/forgotPassword" component={forgotPass}/>
+      <Route exact path="/resetPassword" component={resetPass}/>
     </Router>
     </>
 
